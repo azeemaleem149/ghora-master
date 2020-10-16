d3d9acd15b615d6990ee79118dd59763967685ae
Muhammad Azeem <muhammad.azeem@datum.lahore>
2020-10-16 21:25:04 +0500

    Updates

---
+++ precache-manifest.ab7ca59fdd7dfa8d73a76834ef39a21e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059676f3ee6cfe3bbb9716a3ba9651f4",
+    "url": "/ghora-master/index.html"
+  },
+  {
+    "revision": "666f41af45705eea235d",
+    "url": "/ghora-master/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "bfb0997f5dff248e1ea0",
+    "url": "/ghora-master/static/css/main.b63c5ebf.chunk.css"
+  },
+  {
+    "revision": "666f41af45705eea235d",
+    "url": "/ghora-master/static/js/2.8e38956e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ghora-master/static/js/2.8e38956e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb0997f5dff248e1ea0",
+    "url": "/ghora-master/static/js/main.d612d0ff.chunk.js"
+  },
+  {
+    "revision": "74fb41b10133f1fe62c2",
+    "url": "/ghora-master/static/js/runtime-main.9e24a1f2.js"
+  }
+]);
